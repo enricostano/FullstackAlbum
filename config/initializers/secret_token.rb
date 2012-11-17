ede00e113d3efcc8220e14414d942f6cfe55bca0
enricostano <ocirne.onats@gmail.com>
2012-11-17 13:29:50 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FullstackAlbum::Application.config.secret_token = '94c6dec6aa662072e871f84e0ace37ab23f411a8cb3169261c8c8d583a9769dfe60b14966cc1962abef4e0195a69c7272ac69aca70013828efb690df35a17d87'
