2607606d834b395cc532a643e282db7730db0fa6
enricostano <ocirne.onats@gmail.com>
2012-11-19 16:05:51 +0100

    pass cover image path in JSON to BB.js

--- app/models/album.rb
+++ app/models/album.rb
@@ -4,4 +4,14 @@ class Album < ActiveRecord::Base
   validates_presence_of :name
 
   image_accessor :cover_image
+
+  def as_json(options = {})
+      super(options.merge(:methods => [ :cover_image_path ]))
+  end
+
+  protected
+
+  def cover_image_path
+    self.cover_image ? self.cover_image.url : nil
+  end
 end
